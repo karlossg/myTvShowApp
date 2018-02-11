daea04cc08ad02aa62bc4a1ef442a19715f5137d
karlossg <karol.gratus@gmail.com>
2018-02-11 21:00:38 +0100

    creator controller

---
+++ controllers/creatorController.js
@@ -0,0 +1,41 @@
+const Creator = require('../models/creator');
+
+// Display list of all Creators.
+exports.creator_list = function (req, res) {
+  res.send('NOT IMPLEMENTED: Creator list');
+};
+
+// Display detail page for a specific Creator.
+exports.creator_detail = function (req, res) {
+  res.send('NOT IMPLEMENTED: Creator detail: ' + req.params.id);
+};
+
+// Display Creator create form on GET.
+exports.creator_create_get = function (req, res) {
+  res.send('NOT IMPLEMENTED: Creator create GET');
+};
+
+// Handle Creator create on POST.
+exports.creator_create_post = function (req, res) {
+  res.send('NOT IMPLEMENTED: Creator create POST');
+};
+
+// Display Creator delete form on GET.
+exports.creator_delete_get = function (req, res) {
+  res.send('NOT IMPLEMENTED: Creator delete GET');
+};
+
+// Handle Creator delete on POST.
+exports.creator_delete_post = function (req, res) {
+  res.send('NOT IMPLEMENTED: Creator delete POST');
+};
+
+// Display Creator update form on GET.
+exports.creator_update_get = function (req, res) {
+  res.send('NOT IMPLEMENTED: Creator update GET');
+};
+
+// Handle Creator update on POST.
+exports.creator_update_post = function (req, res) {
+  res.send('NOT IMPLEMENTED: Creator update POST');
+};
