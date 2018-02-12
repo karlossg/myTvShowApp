400a0d232b6b9514ba6a63f499395e5517996c55
karlossg <karol.gratus@gmail.com>
2018-02-12 21:14:25 +0100

    show update pug

--- controllers/showController.js
+++ controllers/showController.js
@@ -86,7 +86,7 @@ exports.show_create_post = [
 
   // Validate fields.
   body('title', 'Title must not be empty.').isLength({ min: 1 }).trim(),
-  body('creator', 'Author must not be empty.').isLength({ min: 1 }).trim(),
+  body('creator', 'Creator must not be empty.').isLength({ min: 1 }).trim(),
   body('summary', 'Summary must not be empty.').isLength({ min: 1 }).trim(),
   // body('imdb_id', 'IMDB ID must not be empty').isLength({ min: 1 }).trim(),
 
@@ -169,10 +169,117 @@ exports.show_delete_post = function (req, res) {
 
 // Display show update form on GET.
 exports.show_update_get = function (req, res) {
-  res.send('NOT IMPLEMENTED: Show update GET');
+  // Get show, creators and genres for form.
+  async.parallel({
+    show: function (callback) {
+      Show.findById(req.params.id).populate('creator').populate('genre').exec(callback);
+    },
+    creators: function (callback) {
+      Creator.find(callback);
+    },
+    genres: function (callback) {
+      Genre.find(callback);
+    },
+  }, function (err, results) {
+    if (err) { return next(err); }
+    if (results.show == null) { // No results.
+      var err = new Error('Show not found');
+      err.status = 404;
+      return next(err);
+    }
+    // Success.
+    // Mark our selected genres as checked.
+    for (var all_g_iter = 0; all_g_iter < results.genres.length; all_g_iter++) {
+      for (var show_g_iter = 0; show_g_iter < results.show.genre.length; show_g_iter++) {
+        if (results.genres[all_g_iter]._id.toString() == results.show.genre[show_g_iter]._id.toString()) {
+          results.genres[all_g_iter].checked = 'true';
+        }
+      }
+    }
+    res.render('show_form', { title: 'Update Show', creators: results.creators, genres: results.genres, show: results.show });
+  });
+
 };
 
 // Handle show update on POST.
-exports.show_update_post = function (req, res) {
-  res.send('NOT IMPLEMENTED: Show update POST');
-};
+exports.show_update_post = [
+
+  // Convert the genre to an array
+  (req, res, next) => {
+    if (!(req.body.genre instanceof Array)) {
+      if (typeof req.body.genre === 'undefined')
+        req.body.genre = [];
+      else
+        req.body.genre = new Array(req.body.genre);
+    }
+    next();
+  },
+
+  // Validate fields.
+  body('title', 'Title must not be empty.').isLength({ min: 1 }).trim(),
+  body('creator', 'Creator must not be empty.').isLength({ min: 1 }).trim(),
+  body('summary', 'Summary must not be empty.').isLength({ min: 1 }).trim(),
+  // body('isbn', 'ISBN must not be empty').isLength({ min: 1 }).trim(),
+
+  // Sanitize fields.
+  sanitizeBody('title').trim().escape(),
+  sanitizeBody('creator').trim().escape(),
+  sanitizeBody('summary').trim().escape(),
+  sanitizeBody('imdb_db').trim().escape(),
+  sanitizeBody('seasons').trim().escape(),
+  sanitizeBody('poster').trim().escape(),
+  sanitizeBody('genre.*').trim().escape(),
+
+  // Process request after validation and sanitization.
+  (req, res, next) => {
+
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    // Create a Show object with escaped/trimmed data and old id.
+    var show = new ShowBook(
+      {
+        title: req.body.title,
+        creator: req.body.creator,
+        summary: req.body.summary,
+        imdb_id: req.body.imdb_id,
+        seasons: req.body.seasons,
+        poster: req.body.poster,
+        genre: (typeof req.body.genre === 'undefined') ? [] : req.body.genre,
+        _id: req.params.id //This is required, or a new ID will be assigned!
+      });
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render form again with sanitized values/error messages.
+
+      // Get all creators and genres for form.
+      async.parallel({
+        creators: function (callback) {
+          Creator.find(callback);
+        },
+        genres: function (callback) {
+          Genre.find(callback);
+        },
+      }, function (err, results) {
+        if (err) { return next(err); }
+
+        // Mark our selected genres as checked.
+        for (let i = 0; i < results.genres.length; i++) {
+          if (show.genre.indexOf(results.genres[i]._id) > -1) {
+            results.genres[i].checked = 'true';
+          }
+        }
+        res.render('show_form', { title: 'Update Show', creators: results.creators, genres: results.genres, show: show, errors: errors.array() });
+      });
+      return;
+    }
+    else {
+      // Data from form is valid. Update the record.
+      Show.findByIdAndUpdate(req.params.id, show, {}, function (err, theshow) {
+        if (err) { return next(err); }
+        // Successful - redirect to show detail page.
+        res.redirect(theshow.url);
+      });
+    }
+  }
+];
