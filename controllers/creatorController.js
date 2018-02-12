0bdd5cdc904767ad2367c389ca05a08dcbd55f55
karlossg <karol.gratus@gmail.com>
2018-02-12 17:46:24 +0100

    add creator controller

--- controllers/creatorController.js
+++ controllers/creatorController.js
@@ -1,4 +1,7 @@
 const async = require('async');
+const { body, validationResult } = require('express-validator/check');
+const { sanitizeBody } = require('express-validator/filter');
+
 const Show = require('../models/show');
 const Creator = require('../models/creator');
 
@@ -39,13 +42,56 @@ exports.creator_detail = function (req, res) {
 
 // Display Creator create form on GET.
 exports.creator_create_get = function (req, res) {
-  res.send('NOT IMPLEMENTED: Creator create GET');
+  res.render('author_form', { title: 'Create Author' });
 };
 
-// Handle Creator create on POST.
-exports.creator_create_post = function (req, res) {
-  res.send('NOT IMPLEMENTED: Creator create POST');
-};
+// Handle Show Creator create on POST.
+exports.creator_create_post = [
+
+  // Validate fields.
+  body('first_name').isLength({ min: 1 }).trim().withMessage('First name must be specified.')
+    .isAlphanumeric().withMessage('First name has non-alphanumeric characters.'),
+  body('last_name').isLength({ min: 1 }).trim().withMessage('Last name must be specified.')
+    .isAlphanumeric().withMessage('Last name has non-alphanumeric characters.'),
+  body('date_of_birth', 'Invalid date of birth').optional({ checkFalsy: true }).isISO8601(),
+  body('date_of_death', 'Invalid date of death').optional({ checkFalsy: true }).isISO8601(),
+
+  // Sanitize fields.
+  sanitizeBody('first_name').trim().escape(),
+  sanitizeBody('last_name').trim().escape(),
+  sanitizeBody('date_of_birth').toDate(),
+  sanitizeBody('date_of_death').toDate(),
+
+  // Process request after validation and sanitization.
+  (req, res, next) => {
+
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render form again with sanitized values/errors messages.
+      res.render('creator_form', { title: 'Add Show Creator', creator: req.body, errors: errors.array() });
+      return;
+    }
+    else {
+      // Data from form is valid.
+
+      // Create an Author object with escaped and trimmed data.
+      const creator = new Creator(
+        {
+          first_name: req.body.first_name,
+          last_name: req.body.last_name,
+          date_of_birth: req.body.date_of_birth,
+          date_of_death: req.body.date_of_death
+        });
+      author.save(function (err) {
+        if (err) { return next(err); }
+        // Successful - redirect to new author record.
+        res.redirect(creator.url);
+      });
+    }
+  }
+];
 
 // Display Creator delete form on GET.
 exports.creator_delete_get = function (req, res) {
--- controllers/genreController.js
+++ controllers/genreController.js
@@ -74,7 +74,7 @@ exports.genre_create_post = [
 
     if (!errors.isEmpty()) {
       // There are errors. Render the form again with sanitized values/error messages.
-      res.render('genre_form', { title: 'Create Genre', genre: genre, errors: errors.array() });
+      res.render('genre_form', { title: 'Add Genre', genre: genre, errors: errors.array() });
       return;
     }
     else {
